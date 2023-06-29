4e4f82645151d84b5b8776ed0466c81ce6540e6a
Danica Gazdić <danicagazdic@gmail.com>
2023-06-29 16:00:11 +0200

    Fixed initialization of backend and added database

---
+++ SCADA_Back/SCADA_Back/Context/SCADA_Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SCADA_Back.Context
+{
+	public class SCADA_Context : DbContext
+	{
+		public SCADA_Context(DbContextOptions<SCADA_Context> options)
+		: base(options)
+		{
+		}
+
+	}
+}
